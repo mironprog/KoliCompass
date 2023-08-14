03f6290909814bdab6acd2a5710326a72eb88e7d
mironprog <bmaresz90@gmail.com>
2023-08-14 19:23:16 +0200

    Edited the login panel

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -7,3 +7,14 @@ function closeLogin()
 {
     document.getElementById('login-div').style.display = "none";
 }
+
+function sendEmail()
+{
+    var link = "mailto:bmaresz90@gmail.com"
+             + "?cc=bmaresz90@gmail.com"
+             + "&subject=" + encodeURIComponent("This is my subject")
+             + "&body=" + encodeURIComponent(document.getElementById('myText').value)
+    ;
+    
+    window.location.href = link
+}
