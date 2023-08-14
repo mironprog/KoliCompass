acc02c63d29e33e788b4243d195807e6b047d998
mironprog <bmaresz90@gmail.com>
2023-08-14 16:16:42 +0200

    Working login button

---
+++ frontend/js/index.js
@@ -0,0 +1,9 @@
+function openLogin() 
+{
+    document.getElementById('login-div').style.display = "block";
+}
+
+function closeLogin()
+{
+    document.getElementById('login-div').style.display = "none";
+}
